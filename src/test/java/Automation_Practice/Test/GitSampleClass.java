54e2080be52610e15a6c9030027787b81bc3c676
Zaker8055 <pmzakerr@gmail.com>
2024-02-22 13:55:35 +0530

    added my new file

---
+++ src/test/java/Automation_Practice/Test/GitSampleClass.java
@@ -0,0 +1,12 @@
+package Automation_Practice.Test;
+
+import org.testng.annotations.Test;
+
+public class GitSampleClass {
+
+	@Test
+	public void gitcode(){
+		
+		System.out.println("GIT code done MR Zaker");
+	}
+}
